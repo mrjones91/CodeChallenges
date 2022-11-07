f8ceb1ee9f0860f6fb2f953598fbc7b1f105db68
DJ <epicmrjones@gmail.com>
2022-11-07 11:37:46 -0600

    init

---
+++ CodeChallenge01/CodeChallenge01/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CodeChallenge01;
+
+Console.WriteLine("Hello, World!");
+int[] ray = { 1, 2, 3, 4, 5, 6 };
+
+Reverse.rev(ray);
---
+++ CodeChallenge01/CodeChallenge01/Reverse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge01
+{
+    internal class Reverse
+    {
+        public static void rev(int[] ray)
+        {
+            int[] revray = new int[ray.Length];
+            int counter = 0;
+
+            for (int i = ray.Length - 1; i >= 0; i--)
+            {
+                revray[counter] = ray[i];
+                counter++;
+                
+            }
+
+            foreach (int i in revray)
+            {
+                Console.Write(i);
+            }
+        }
+    }
+}
