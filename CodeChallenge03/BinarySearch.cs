183103a88f2dc14f57f8cae8f6c13027ac65fe71
DJ <epicmrjones@gmail.com>
2022-11-09 20:53:45 -0600

    Recursive Binary Search

---
+++ CodeChallenge03/BinarySearch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge03
+{
+	public class BinarySearch
+	{
+		public static void Search(int[] ray, int target) 
+		{
+			int divider = 0, mid = 0;
+			int min = 0, max = ray.Length;
+
+			//if (ray.Length % 2 != 0)
+			//	divider = 1;
+			//mid = (ray.Length / 2) + divider;
+
+			mid = (max + min) / 2;
+
+			Console.WriteLine("Checking for " + target + " at [" + mid + "] from ray[" + min + "] to ray[" + max + "]");
+			for (int r = 0; r < max; r++) {
+				Console.WriteLine("[" + r + "]: " + ray[r]);
+			}
+			
+			Console.WriteLine("");
+
+			if (target == ray[mid]) {
+				Console.WriteLine("Woot!");
+				Console.WriteLine("Target: " + target + " found at ray[" + mid + "]\n\n");
+				return;
+			} else if (target > ray[mid]) {
+				min = mid;
+				
+				Console.WriteLine("Target is higher than middle value");
+				
+			}
+			else { //target < ray[mid]
+				max = mid;
+				
+				Console.WriteLine("Target is lower than middle value");
+			}
+
+			int[] newRay = new int[max - min];
+			Array.Copy(ray, min, newRay, 0, newRay.Length );
+
+			
+			Console.WriteLine("\n------------\n");
+			Search(newRay, target);
+		}
+	}
+}
---
+++ CodeChallenge03/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CodeChallenge03;
+
+Console.WriteLine("Hello, World!");
+int[] demo = { 10, 35, 42, 55, 77 };
+int[] d2 = { 2, 4, 6, 8, 10 };
+BinarySearch.Search(d2, 8);
